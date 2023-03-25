5b3ee1175c57f057bea18afdee07b00453d8c589
Axizeaus <axizeaus@gmail.com>
2023-03-25 12:19:45 +0630

    forecast data showing now

--- src/index.js
+++ src/index.js
@@ -8,55 +8,7 @@ async function getCurrentWeatherData(city='london'){
   return respJson;
 }
 
-async function showCurrentWeatherData(cityName){
-  const content = document.getElementById('content');
-  const currentData = await getCurrentWeatherData(cityName);
-  const trimmedData = await trimCurrentData(currentData);
-  trimmedData.forEach((data) => {
-    const div = document.createElement('div');
-    div.innerHTML = data;
-    content.append(div);
-  });
-}
-
-async function getForecast(city='london'){
-  const resp = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=b00ef59dc62e4d45bcf61559232003&q=${city}&days=8&aqi=no&alerts=no`
-  );
-  const respJson = await resp.json();
-  return respJson;
-}
-
-async function foreCastTrimPerDay(respDay){
-  const resp = await respDay;
-  const date = resp.date;
-  const day = resp.day;
-
-  const averageTempInC = day.avgtemp_c;
-  const averageTempInF = day.avgtemp_f;
-  const maxWindMph = day.maxwind_mph;
-  const maxWindKph = day.maxwind_kph;
-  const avgHumidity = day.avghumidity;
-  const condition = day.condition.text;
-
-  console.log(date);
-  console.log(day);
-  console.log(averageTempInC);
-  console.log(averageTempInF);
-  console.log(maxWindMph);
-  console.log(maxWindKph);
-  console.log(avgHumidity);
-  console.log(condition);
-}
-
-async function trimForecastData(respJson){
-  const resp = await respJson;
-  const sevenDays = resp.forecast.forecastday;
-  sevenDays.forEach(foreCastTrimPerDay);
-}
-
-async function trimCurrentData(respJson){
-
+async function trimCurrentData(respJson) {
   const resp = await respJson;
   const tempInC = resp.current.temp_c;
   const tempInF = resp.current.temp_f;
@@ -75,22 +27,10 @@ async function trimCurrentData(respJson){
 
   const cloud = resp.current.cloud;
   const humidity = resp.current.humidity;
-
-  console.log(resp);
-  console.log(tempInC);
-  console.log(tempInF);
-  console.log(lastUpdated);
-  console.log(feelsLikeC);
-  console.log(feelsLikeF);
-  console.log(country, city);
-  console.log(condition);
-  console.log(windDir, windKph, windMph)
-  console.log(cloud);
-  console.log('humidity: ', humidity);
   return [
     country,
     city,
-    condition, 
+    condition,
     lastUpdated,
     tempInC,
     tempInF,
@@ -101,16 +41,77 @@ async function trimCurrentData(respJson){
     windDir,
     windKph,
     windMph,
-  ]
-  
+  ];
+}
+
+
+async function showCurrentWeatherData(cityName){
+  const content = document.getElementById('content');
+  const currentData = await getCurrentWeatherData(cityName);
+  const trimmedData = await trimCurrentData(currentData);
+  trimmedData.forEach((data) => {
+    const div = document.createElement('div');
+    div.innerHTML = data;
+    content.append(div);
+  });
+}
+
+async function getForecast(city = "london") {
+  const resp = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=b00ef59dc62e4d45bcf61559232003&q=${city}&days=8&aqi=yes&alerts=no`
+  );
+  const respJson = await resp.json();
+  return respJson;
+}
+
+async function trimForecastData(respJson) {
+  const resp = await respJson;
+  const sevenDays = resp.forecast.forecastday;
+  const sevenDaysData = await sevenDays.map(item => foreCastTrimPerDay(item));
+  return sevenDaysData;
+}
+
+
+function foreCastTrimPerDay(respDay) {
+  const resp = respDay;
+  const date = resp.date;
+  const day = resp.day;
+
+
+  const averageTempInC = day.avgtemp_c;
+  const averageTempInF = day.avgtemp_f;
+  const maxWindMph = day.maxwind_mph;
+  const maxWindKph = day.maxwind_kph;
+  const avgHumidity = day.avghumidity;
+  const condition = day.condition.text;
+
+  return [
+    date,
+    averageTempInC,
+    averageTempInF,
+    maxWindKph,
+    maxWindMph,
+    avgHumidity,
+    condition,
+  ];
+}
+
+async function showForeCastWeatherData(cityName){
+  const forecast = document.getElementById('forecast');
+  const forecastData = await getForecast(cityName);
+  const trimmedData = await trimForecastData(forecastData);
+  trimmedData.forEach((data) => {
+    const li = document.createElement('li');
+    li.innerHTML = data;
+    forecast.append(li);
+  })
 }
 
 function getCityName(event){
   event.preventDefault();
   const city = document.getElementById('cityName');
   showCurrentWeatherData(city.value);
-  // const foreCastData = getForecast(city.value);
-  // trimForecastData (foreCastData);
+  showForeCastWeatherData(city.value);
   city.value = '';
 }
 
@@ -118,3 +119,8 @@ const form = document.getElementById('form');
 form.addEventListener('submit', function(e){
   getCityName(e);
 })
+
+window.onload = () =>{
+  showCurrentWeatherData('lashio');
+  showForeCastWeatherData('lashio');
+}
